548caf84d428fea75fe4c01455a5834bdc16d60b
theachoem <theacheng.g6@gmail.com>
2022-01-05 23:25:20 +0700

    add enum preference storage

---
+++ test/core/base_storages/enum_preference_storage_test.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:spooky/core/base_storages/enum_preference_storage.dart';
+
+class FakeEnumStorage extends EnumPreferenceStorage<FakeType> {
+  @override
+  List<FakeType> get values => FakeType.values;
+}
+
+enum FakeType {
+  ac,
+  bc,
+}
+
+void main() {
+  SharedPreferences.setMockInitialValues({});
+  FakeEnumStorage storage = FakeEnumStorage();
+  group('FakeEnumStorage', () {
+    test('return FakeType.bc if enum value is set', () async {
+      await storage.writeEnum(FakeType.bc);
+      FakeType? result = await storage.readEnum();
+      expect(result, FakeType.bc);
+    });
+
+    test('return null if storage was cleared', () async {
+      await storage.remove();
+      FakeType? result = await storage.readEnum();
+      expect(result, null);
+    });
+  });
+}
