835f3cb9119283fa9fe4c170e200bb6ab48fec98
theachoem <theacheng.g6@gmail.com>
2022-01-05 22:31:44 +0700

    set initial theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:spooky/app.dart';
+import 'package:spooky/theme/theme_constant.dart';
 import 'package:spooky/utils/constants/app_constant.dart';
 import 'package:spooky/core/locator.dart';
 import 'package:flutter/material.dart';
@@ -14,7 +15,23 @@ void main() async {
       supportedLocales: AppConstant.supportedLocales,
       fallbackLocale: AppConstant.fallbackLocale,
       path: 'assets/translations',
-      child: App(),
+      child: InitialTheme(),
     ),
   );
+}
+
+class InitialTheme extends StatelessWidget {
+  const InitialTheme({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.system,
+      theme: ThemeData(colorScheme: ThemeConstant.lightM3Color.toColorScheme()),
+      darkTheme: ThemeData(colorScheme: ThemeConstant.darkM3Color.toColorScheme()),
+      home: App(),
+    );
+  }
 }
