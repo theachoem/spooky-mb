b08c50bb2449c3abdc9ef2b59e0d8745130dfc57
theachoem <theacheng.g6@gmail.com>
2022-01-06 19:11:30 +0700

    extract chip to global widget

--- lib/ui/views/home/local_widgets/story_list.dart
+++ lib/ui/views/home/local_widgets/story_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:spooky/theme/m3/m3_color.dart';
 import 'package:spooky/theme/m3/m3_text_theme.dart';
+import 'package:spooky/ui/widgets/sp_chip.dart';
 import 'package:spooky/ui/widgets/sp_tap_effect.dart';
 import 'package:spooky/utils/constants/config_constant.dart';
 
@@ -100,40 +101,6 @@ class StoryList extends StatelessWidget {
           child: Text('14'),
         ),
       ],
-    );
-  }
-}
-
-class SpChip extends StatelessWidget {
-  const SpChip({
-    Key? key,
-    required this.labelText,
-    this.avatar,
-  }) : super(key: key);
-
-  final String labelText;
-  final Widget? avatar;
-
-  @override
-  Widget build(BuildContext context) {
-    return SpTapEffect(
-      effects: const [SpTapEffectType.scaleDown],
-      onTap: () {},
-      child: Chip(
-        label: Text(
-          labelText,
-          style: M3TextTheme.of(context)?.labelLarge,
-        ),
-        backgroundColor: Colors.transparent,
-        shape: RoundedRectangleBorder(
-          borderRadius: ConfigConstant.circlarRadius1,
-          side: BorderSide(
-            color: M3Color.of(context)!.outline,
-          ),
-        ),
-        padding: const EdgeInsets.all(ConfigConstant.margin1),
-        avatar: avatar,
-      ),
     );
   }
 }
