712cfaba07663a4960fa41d267dc8aa4d4e1bfba
Thea Choem <theacheng.g6@gmail.com>
2022-02-25 22:01:07 +0700

    close #41 setup firebase

--- commands.md
+++ commands.md
@@ -1,3 +1,7 @@
 ```s
 fvm flutter pub run build_runner build --delete-conflicting-outputs
 ```
+
+```s
+flutterfire configure
+```
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,6 +8,7 @@ import Foundation
 import audioplayers
 import desktop_window
 import device_info_plus_macos
+import firebase_core
 import flutter_local_notifications
 import flutter_secure_storage_macos
 import package_info_plus_macos
@@ -19,6 +20,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersPlugin.register(with: registry.registrar(forPlugin: "AudioplayersPlugin"))
   DesktopWindowPlugin.register(with: registry.registrar(forPlugin: "DesktopWindowPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStorageMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageMacosPlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
