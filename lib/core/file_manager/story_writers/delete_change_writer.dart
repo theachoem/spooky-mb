e8e203c8ed4ecb164d572111bf12da410a08c43f
Thea Choem <theacheng.g6@gmail.com>
2022-02-26 16:16:47 +0700

    implement delete changes writer

--- lib/core/file_manager/story_writers/delete_change_writer.dart
+++ lib/core/file_manager/story_writers/delete_change_writer.dart
@@ -1,29 +1,12 @@
-import 'dart:io';
-import 'package:spooky/core/file_manager/story_writers/base_story_writer.dart';
+import 'package:spooky/core/file_manager/story_writers/default_story_writer.dart';
 import 'package:spooky/core/file_manager/story_writers/objects/delete_change_object.dart';
 import 'package:spooky/core/models/story_model.dart';
-import 'package:spooky/core/types/response_code_type.dart';
 
-class DeleteChangeWriter extends BaseStoryWriter<DeleteChangeObject> {
-  @override
-  String buildMessage(ResponseCodeType responseCode) {
-    // TODO: implement buildMessage
-    throw UnimplementedError();
-  }
-
-  @override
-  void onSaved({
-    required StoryModel? story,
-    required FileSystemEntity? file,
-    required ResponseCodeType responseCode,
-    required String message,
-  }) {
-    // TODO: implement onSaved
-  }
-
+class DeleteChangeWriter extends DefaultStoryWriter<DeleteChangeObject> {
   @override
   StoryModel buildStory(DeleteChangeObject object) {
-    // TODO: implement buildStory
-    throw UnimplementedError();
+    StoryModel story = object.viewModel.currentStory;
+    story.removeChangeByIds(object.contentIds);
+    return story;
   }
 }
--- lib/core/file_manager/story_writers/objects/delete_change_object.dart
+++ lib/core/file_manager/story_writers/objects/delete_change_object.dart
@@ -1,7 +1,8 @@
 import 'package:spooky/core/file_manager/story_writers/objects/base_writer_object.dart';
+import 'package:spooky/core/file_manager/story_writers/objects/default_story_object.dart';
 import 'package:spooky/ui/views/detail/detail_view_model.dart';
 
-class DeleteChangeObject extends BaseWriterObject {
+class DeleteChangeObject extends DefaultStoryObject {
   final List<String> contentIds;
   DeleteChangeObject(
     DetailViewModel viewModel, {
