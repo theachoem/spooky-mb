1f6cbe2334f1518b126cdfcc607b1f660d01f92f
Thea Choem <theacheng.g6@gmail.com>
2022-02-26 16:27:45 +0700

    implement restore story writer

--- lib/core/file_manager/story_writers/objects/restore_story_object.dart
+++ lib/core/file_manager/story_writers/objects/restore_story_object.dart
@@ -1,7 +1,8 @@
 import 'package:spooky/core/file_manager/story_writers/objects/base_writer_object.dart';
+import 'package:spooky/core/file_manager/story_writers/objects/default_story_object.dart';
 import 'package:spooky/ui/views/detail/detail_view_model.dart';
 
-class RestoreStoryObject extends BaseWriterObject {
+class RestoreStoryObject extends DefaultStoryObject {
   RestoreStoryObject(
     DetailViewModel viewModel, {
     required this.contentId,
--- lib/core/file_manager/story_writers/restore_story_writer.dart
+++ lib/core/file_manager/story_writers/restore_story_writer.dart
@@ -1,14 +1,24 @@
 import 'dart:io';
-import 'package:spooky/core/file_manager/story_writers/base_story_writer.dart';
+import 'package:flutter/material.dart';
+import 'package:spooky/core/file_manager/story_writers/default_story_writer.dart';
 import 'package:spooky/core/file_manager/story_writers/objects/restore_story_object.dart';
+import 'package:spooky/core/models/story_content_model.dart';
 import 'package:spooky/core/models/story_model.dart';
+import 'package:spooky/core/routes/sp_route_config.dart';
+import 'package:spooky/core/types/detail_view_flow_type.dart';
 import 'package:spooky/core/types/response_code_type.dart';
 
-class RestoreStoryWriter extends BaseStoryWriter<RestoreStoryObject> {
+class RestoreStoryWriter extends DefaultStoryWriter<RestoreStoryObject> {
   @override
   String buildMessage(ResponseCodeType responseCode) {
-    // TODO: implement buildMessage
-    throw UnimplementedError();
+    switch (responseCode) {
+      case ResponseCodeType.success:
+        return "Restored";
+      case ResponseCodeType.noChange:
+        return "Document has no changes";
+      case ResponseCodeType.fail:
+        return "Restore unsuccessfully!";
+    }
   }
 
   @override
@@ -18,12 +28,33 @@ class RestoreStoryWriter extends BaseStoryWriter<RestoreStoryObject> {
     required ResponseCodeType responseCode,
     required String message,
   }) {
-    // TODO: implement onSaved
+    super.onSaved(
+      story: story,
+      file: file,
+      responseCode: responseCode,
+      message: message,
+    );
+
+    BuildContext? context = super.context;
+    if (context == null || story == null) return;
+
+    WidgetsBinding.instance?.addPostFrameCallback((timeStamp) {
+      Navigator.of(context).popAndPushNamed(
+        SpRouteConfig.detail,
+        arguments: DetailArgs(
+          initialStory: story,
+          intialFlow: DetailViewFlowType.update,
+        ),
+      );
+    });
   }
 
   @override
   StoryModel buildStory(RestoreStoryObject object) {
-    // TODO: implement buildStory
-    throw UnimplementedError();
+    StoryModel story = super.buildStory(object);
+    StoryContentModel content = object.viewModel.currentContent;
+    story.removeChangeById(content.id);
+    story.addChange(content.restore(content));
+    return story;
   }
 }
