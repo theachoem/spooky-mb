3177152aec9c6e70227ae19e0299198cbbc13919
Thea Choem <theacheng.g6@gmail.com>
2022-03-01 16:11:27 +0700

    add base architecture

---
+++ lib/core/base/view_model_provider.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:spooky/core/base/base_view_model.dart';
+
+class ViewModelProvider<T extends BaseViewModel> extends StatelessWidget {
+  const ViewModelProvider({
+    Key? key,
+    required this.builder,
+    required this.create,
+    this.onModelReady,
+    this.child,
+  }) : super(key: key);
+
+  final Create<T> create;
+  final Widget? child;
+  final void Function(T viewModel)? onModelReady;
+  final Widget Function(BuildContext context, T viewModel, Widget? child) builder;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListenableProvider<T>(
+      create: (BuildContext context) => create(context),
+      child: child,
+      builder: (context, child) {
+        T viewModel = Provider.of<T>(context);
+        if (onModelReady != null) {
+          WidgetsBinding.instance?.addPostFrameCallback((timeStamp) {
+            onModelReady!(viewModel);
+          });
+        }
+        return builder(context, viewModel, child);
+      },
+    );
+  }
+}
