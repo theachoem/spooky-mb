de3e72d1bf305948dff1fb145442bddc0d20df3e
Thea Choem <theacheng.g6@gmail.com>
2022-06-23 21:58:11 +0700

    close #134 blank notification icon

--- lib/core/notification/channels/base_notification_channel.dart
+++ lib/core/notification/channels/base_notification_channel.dart
@@ -4,6 +4,7 @@ import 'package:spooky/app.dart';
 import 'package:spooky/core/types/notification_channel_types.dart';
 import 'package:spooky/core/notification/notification_service.dart';
 import 'package:spooky/core/notification/payloads/base_notification_payload.dart';
+import 'package:spooky/theme/m3/m3_color.dart';
 
 abstract class BaseNotificationChannel<T extends BaseNotificationPayload> {
   NotificationChannelTypes get channelKey;
@@ -26,15 +27,21 @@ abstract class BaseNotificationChannel<T extends BaseNotificationPayload> {
     NotificationLayout? notificationLayout,
   }) {
     notificationLayout ??= bigPicture != null ? NotificationLayout.BigPicture : NotificationLayout.Default;
+    BuildContext? context = App.navigatorKey.currentContext;
+    ColorScheme? colorScheme = context != null ? M3Color.of(context) : null;
+
     NotificationContent content = NotificationContent(
       id: title.hashCode,
       channelKey: channelKey.name,
       title: title,
       body: body,
+      color: colorScheme?.onPrimary,
+      backgroundColor: colorScheme?.primary,
       bigPicture: bigPicture,
       notificationLayout: notificationLayout,
       payload: payload?.toPayload(),
     );
+
     return NotificationService.notifications.createNotification(
       content: content,
       actionButtons: actionButtons,
