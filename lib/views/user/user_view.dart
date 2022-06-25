9d0e7b29b52c1ddb64c80494c5d9ad7e87a37c2c
Thea Choem <theacheng.g6@gmail.com>
2022-06-25 17:35:13 +0700

    refactor: extract code to external file

--- lib/views/user/user_view.dart
+++ lib/views/user/user_view.dart
@@ -1,20 +1,17 @@
 library user_view;
 
 import 'package:adaptive_dialog/adaptive_dialog.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:spooky/core/base/view_model_provider.dart';
 import 'package:spooky/core/routes/sp_router.dart' as router;
 import 'package:spooky/core/services/messenger_service.dart';
 import 'package:spooky/theme/m3/m3_color.dart';
-import 'package:spooky/utils/constants/config_constant.dart';
-import 'package:spooky/utils/helpers/date_format_helper.dart';
+import 'package:spooky/views/user/local_widgets/user_image_profile.dart';
 import 'package:spooky/widgets/sp_animated_icon.dart';
 import 'package:spooky/widgets/sp_app_bar_title.dart';
 import 'package:spooky/widgets/sp_icon_button.dart';
 import 'package:spooky/widgets/sp_pop_button.dart';
-import 'package:spooky/widgets/sp_pop_up_menu_button.dart';
 import 'package:spooky/widgets/sp_sections_tiles.dart';
 import 'package:swipeable_page_route/swipeable_page_route.dart';
 import 'user_view_model.dart';
