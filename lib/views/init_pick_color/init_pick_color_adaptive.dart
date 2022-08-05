4497cf567d2d511d937510cef295e6f8431d2618
Thea Choem <theacheng.g6@gmail.com>
2022-08-05 20:34:25 +0700

    add init color picker localization

--- lib/views/init_pick_color/init_pick_color_adaptive.dart
+++ lib/views/init_pick_color/init_pick_color_adaptive.dart
@@ -70,8 +70,8 @@ class _InitPickColorAdaptive extends StatelessWidget {
                             color: Theme.of(context).dividerColor,
                           ),
                         ),
-                        title: const Text("Dynamic color"),
-                        subtitle: const Text("Use colors base on your system."),
+                        title: Text(tr("tile.dynamic_color.title")),
+                        subtitle: Text(tr("tile.dynamic_color.subtitle")),
                         value: Provider.of<ThemeProvider>(context, listen: false).themeMode == ThemeMode.system,
                         onChanged: (bool value) {
                           final provider = Provider.of<ThemeProvider>(context, listen: false);
