4d86ac5d054e183f457ee92f9c66e8c2af6af82c
Thea Choem <29684683+theachoem@users.noreply.github.com>
2024-12-22 03:02:51 +0700

    Improve story tile & render markdown for body

--- lib/widgets/story_list/story_tile.dart
+++ lib/widgets/story_list/story_tile.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:spooky/core/databases/models/story_content_db_model.dart';
 import 'package:spooky/core/databases/models/story_db_model.dart';
 import 'package:spooky/core/services/color_from_day_service.dart';
 
 class StoryTile extends StatelessWidget {
-  static const double circleSize = 32;
+  static const double monogramSize = 32;
 
   const StoryTile({
     super.key,
@@ -20,28 +21,79 @@ class StoryTile extends StatelessWidget {
     StoryContentDbModel? content = story.changes.lastOrNull;
     String? displayBody = content != null ? getDisplayBodyFor(content) : null;
 
-    return MediaQuery.removePadding(
-      removeLeft: true,
-      removeRight: true,
-      context: context,
-      child: ListTile(
-        leading: Container(
-          width: circleSize,
-          height: circleSize,
-          decoration: BoxDecoration(
-            color: ColorFromDayService(context: context).get(story.displayPathDate.weekday),
-            shape: BoxShape.circle,
-          ),
-          alignment: Alignment.center,
-          child: Text(
-            story.displayPathDate.day.toString(),
-            style: Theme.of(context).textTheme.labelMedium?.copyWith(color: Theme.of(context).colorScheme.onPrimary),
+    bool hasTitle = content?.title?.trim().isNotEmpty == true;
+    bool hasBody = displayBody != null && displayBody.trim().length > 1;
+
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          spacing: 16.0,
+          children: [
+            buildMonogram(context),
+            Expanded(
+              child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                if (hasTitle)
+                  Text(
+                    content!.title!,
+                    style: TextTheme.of(context).titleMedium,
+                    maxLines: 3,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                if (hasBody) buildMarkdownBody(displayBody, context)
+              ]),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildMonogram(BuildContext context) {
+    return Container(
+      width: monogramSize,
+      height: monogramSize,
+      decoration: BoxDecoration(
+        color: ColorFromDayService(context: context).get(story.displayPathDate.weekday),
+        shape: BoxShape.circle,
+      ),
+      alignment: Alignment.center,
+      child: Text(
+        story.displayPathDate.day.toString(),
+        style: Theme.of(context).textTheme.labelMedium?.copyWith(color: Theme.of(context).colorScheme.onPrimary),
+      ),
+    );
+  }
+
+  Widget buildMarkdownBody(String displayBody, BuildContext context) {
+    return MarkdownBody(
+      data: displayBody,
+      onTapLink: (url, _, __) {},
+      styleSheet: MarkdownStyleSheet(
+        blockquoteDecoration: BoxDecoration(
+          color: Colors.transparent,
+          border: Border(
+            left: BorderSide(color: Theme.of(context).dividerColor),
           ),
         ),
-        title: Text(content?.title ?? 'N/A'),
-        subtitle: displayBody != null ? Text(displayBody) : null,
-        onTap: onTap,
+        blockquotePadding: const EdgeInsets.symmetric(vertical: 0, horizontal: 8.0),
+        codeblockDecoration: BoxDecoration(border: Border.all(color: Theme.of(context).dividerColor)),
+        listBulletPadding: const EdgeInsets.all(2),
+        listIndent: 16,
+        blockSpacing: 0.0,
       ),
+      checkboxBuilder: (checked) {
+        return Transform.translate(
+          offset: const Offset(-3.5, 2.5),
+          child: Icon(
+            checked ? Icons.check_box : Icons.check_box_outline_blank,
+            size: 16.0,
+          ),
+        );
+      },
+      softLineBreak: true,
     );
   }
 
